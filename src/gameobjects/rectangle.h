87e567be7f3f74aaefb7aa2d5e8d6cddf2e367db
Elias <eliasbrucart@hardgames.com.ar>
2020-03-17 03:30:09 -0300

    Added rectangle class

--- src/gameobjects/rectangle.h
+++ src/gameobjects/rectangle.h
@@ -3,7 +3,26 @@
 
 namespace Colortrack 
 {
-
+	class Rectangle
+	{
+	private:
+		int _width;
+		int _height;
+		int _x;
+		int _y;
+	public:
+		void InitRectangleEnemy();
+		void DrawRectangleEnemy();
+		void MoveRectangleEnemy();
+		void SetX();
+		int GetX(int x);
+		void SetY();
+		int GetY(int y);
+		void SetWidth();
+		int GetWidth(int width);
+		void SetHeight();
+		int GetHeight(int height);
+	};
 }
 
 #endif
