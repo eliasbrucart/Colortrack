b91e2c7391ee10e23b42e32b56d0fa204964b282
Elias <eliasbrucart@hardgames.com.ar>
2020-03-17 04:10:48 -0300

    Completed structure of the rectangle class

--- src/gameobjects/rectangle.cpp
+++ src/gameobjects/rectangle.cpp
@@ -1,4 +1,59 @@
+#include "rectangle.h"
+
 namespace Colortrack
 {
+	void Rectangle::InitRectangleEnemy()
+	{
 
+	}
+
+	void Rectangle::DrawRectangleEnemy()
+	{
+
+	}
+
+	void Rectangle::MoveRectangleEnemy()
+	{
+
+	}
+
+	void Rectangle::SetX()
+	{
+
+	}
+
+	int Rectangle::GetX(int x)
+	{
+
+	}
+
+	void Rectangle::SetY()
+	{
+
+	}
+
+	int Rectangle::GetY(int y)
+	{
+
+	}
+
+	void Rectangle::SetWidth()
+	{
+
+	}
+
+	int Rectangle::GetWidth(int width) 
+	{
+
+	}
+
+	void Rectangle::SetHeight()
+	{
+
+	}
+
+	int Rectangle::GetHeight(int height)
+	{
+
+	}
 }
