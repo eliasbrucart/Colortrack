00c3573706ff008ac11607b3f40b29363d5be5bf
Elias <eliasbrucart@hardgames.com.ar>
2020-05-02 14:30:19 -0300

    Minimal main

--- src/main.cpp
+++ src/main.cpp
@@ -7,27 +7,4 @@ int main()
 	Loop();
 
 	return 0;
-	/*InitWindow(640, 480, "Colortrack");
-	Rectangle player;
-	player.x = 200.0f;
-	player.y = 200.0f;
-	player.width = 30.0f;
-	player.height = 30.0f;
-	while (!WindowShouldClose())
-	{
-		BeginDrawing();
-		ClearBackground(BLACK);
-		if (IsKeyDown(KEY_A))
-		{
-			player.x -= 600.0f * GetFrameTime();
-		}
-		if (IsKeyDown(KEY_D))
-		{
-			player.x += 600.0f * GetFrameTime();
-		}
-		
-		DrawRectangle(player.x, player.y, player.width, player.height, WHITE);
-		EndDrawing();
-	}*/
-
 }
