251ea973b28d97e555a84aa1489f269f63cc7bb3
Elias <eliasbrucart@hardgames.com.ar>
2020-07-09 21:57:14 -0300

    Basic structure of gameplay class

--- src/screens/gameplay.cpp
+++ src/screens/gameplay.cpp
@@ -1,1 +1,24 @@
 #include "gameplay.h"
+
+namespace Colortrack
+{
+	Gameplay::Gameplay()
+	{
+
+	}
+
+	Gameplay::~Gameplay()
+	{
+
+	}
+
+	void Gameplay::Update()
+	{
+
+	}
+
+	void Gameplay::Draw()
+	{
+
+	}
+}
--- src/screens/gameplay.h
+++ src/screens/gameplay.h
@@ -1,4 +1,16 @@
 #ifndef GAMEPLAY_H
 #define GAMEPLAY_H
 
+namespace Colortrack
+{
+	class Gameplay
+	{
+	public:
+		Gameplay();
+		~Gameplay();
+		void Update();
+		void Draw();
+	};
+}
+
 #endif
