3cbc285309fb9615ba4f5771182b40b656ec237a
Elias <eliasbrucart@hardgames.com.ar>
2020-07-09 21:50:27 -0300

    Basic structure of menu class

---
+++ src/screens/menu.cpp
@@ -0,0 +1,21 @@
+#include "menu.h"
+
+Menu::Menu()
+{
+
+}
+
+Menu::~Menu() 
+{
+
+}
+
+void Menu::Draw()
+{
+
+}
+
+void Menu::Input()
+{
+
+}
--- src/screens/menu.h
+++ src/screens/menu.h
@@ -1,1 +1,13 @@
-#pragma once
+#ifndef MENU_H
+#define MENU_H
+
+class Menu 
+{
+public:
+	Menu();
+	~Menu();
+	void Draw();
+	void Input();
+};
+
+#endif
