19587449a9e17efe94019752a75d2f6219bd5b8f
Elias <eliasbrucart@hardgames.com.ar>
2020-07-11 20:39:47 -0300

    Added credits screen

---
+++ src/screens/credits.cpp
@@ -0,0 +1,24 @@
+#include "credits.h"
+
+namespace Colortrack
+{
+	Credits::Credits()
+	{
+
+	}
+
+	Credits::~Credits()
+	{
+
+	}
+
+	void Credits::Input()
+	{
+
+	}
+
+	void Credits::Draw()
+	{
+
+	}
+}
---
+++ src/screens/credits.h
@@ -0,0 +1,16 @@
+#ifndef CREDITS_H
+#define CREDITS_H
+
+namespace Colortrack
+{
+	class Credits
+	{
+	public:
+		Credits();
+		~Credits();
+		void Input();
+		void Draw();
+	};
+}
+
+#endif
