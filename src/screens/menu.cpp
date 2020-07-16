dc771a46e81dbd2b2edffc55a64e09d462ef2ea9
Elias <eliasbrucart@hardgames.com.ar>
2020-07-15 23:51:46 -0300

    Added menu buttons

--- src/screens/menu.cpp
+++ src/screens/menu.cpp
@@ -2,9 +2,36 @@
 
 namespace Colortrack
 {
+	static float xPlayButton = 200.0f;
+	static float yPlayButton = 150.0f;
+	static float widthPlayButton = 100.0f;
+	static float heightPlayButton = 50.0f;
+
+	static float xTutorialButton = 300.0f;
+	static float yTutorialButton = 250.0f;
+	static float widthTutorialButton = 100.0f;
+	static float heightTutorialButton = 50.0f;
+
+	static float xExitButton = 400.0f;
+	static float yExitButton = 350.0f;
+	static float widthExitButton = 100.0f;
+	static float heightExitButton = 50.0f;
 	Menu::Menu()
 	{
+		_playButton.x = xPlayButton;
+		_playButton.y = yPlayButton;
+		_playButton.width = widthPlayButton;
+		_playButton.height = heightPlayButton;
 
+		_tutorialButton.x = xTutorialButton;
+		_tutorialButton.y = yTutorialButton;
+		_tutorialButton.width = widthTutorialButton;
+		_tutorialButton.height = heightTutorialButton;
+
+		_exitButton.x = xExitButton;
+		_exitButton.y = yExitButton;
+		_exitButton.width = widthExitButton;
+		_exitButton.height = heightExitButton;
 	}
 
 	Menu::~Menu()
--- src/screens/menu.h
+++ src/screens/menu.h
@@ -1,10 +1,16 @@
 #ifndef MENU_H
 #define MENU_H
 
+#include "raylib.h"
+
 namespace Colortrack 
 {
 	class Menu
 	{
+	private:
+		Rectangle _playButton;
+		Rectangle _tutorialButton;
+		Rectangle _exitButton;
 	public:
 		Menu();
 		~Menu();
