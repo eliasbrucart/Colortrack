91de7c3b45f5ddf598d2b28c3ad0396a005af4d8
Elias <eliasbrucart@hardgames.com.ar>
2020-07-16 23:06:22 -0300

    We get the mouse in the menu

--- src/screens/menu.cpp
+++ src/screens/menu.cpp
@@ -2,20 +2,22 @@
 
 namespace Colortrack
 {
-	static float xPlayButton = 200.0f;
-	static float yPlayButton = 150.0f;
+	static float xPlayButton = 300.0f;
+	static float yPlayButton = 100.0f;
 	static float widthPlayButton = 100.0f;
 	static float heightPlayButton = 50.0f;
 
 	static float xTutorialButton = 300.0f;
-	static float yTutorialButton = 250.0f;
+	static float yTutorialButton = 200.0f;
 	static float widthTutorialButton = 100.0f;
 	static float heightTutorialButton = 50.0f;
 
-	static float xExitButton = 400.0f;
-	static float yExitButton = 350.0f;
+	static float xExitButton = 300.0f;
+	static float yExitButton = 300.0f;
 	static float widthExitButton = 100.0f;
 	static float heightExitButton = 50.0f;
+
+	static Vector2 mouse;
 	Menu::Menu()
 	{
 		_playButton.x = xPlayButton;
@@ -42,12 +44,12 @@ namespace Colortrack
 	void Menu::Draw()
 	{
 		DrawRectangle(_playButton.x, _playButton.y, _playButton.width, _playButton.height, GREEN);
-		DrawRectangle(_tutorialButton.x, _tutorialButton.y, _tutorialButton.width, _tutorialButton.height, GREEN);
-		DrawRectangle(_exitButton.x, _exitButton.y, _exitButton.width, _exitButton.height, GREEN);
+		DrawRectangle(_tutorialButton.x, _tutorialButton.y, _tutorialButton.width, _tutorialButton.height, YELLOW);
+		DrawRectangle(_exitButton.x, _exitButton.y, _exitButton.width, _exitButton.height, RED);
 	}
 
 	void Menu::Input()
 	{
-
+		mouse = GetMousePosition();
 	}
 }
