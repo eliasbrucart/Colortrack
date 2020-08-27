46671760e75cb837d60a84b840f907fc7fdff409
Elias <eliasbrucart@hardgames.com.ar>
2020-08-26 23:38:56 -0300

    Added basic structure of circle enemy class

--- src/gameobjects/circle_enemy.cpp
+++ src/gameobjects/circle_enemy.cpp
@@ -1,1 +1,74 @@
 #include "circle_enemy.h"
+
+namespace Colortrack
+{
+	CircleEnemy::CircleEnemy()
+	{
+
+	}
+
+	CircleEnemy::CircleEnemy(float x, float y, float radius, Vector2 center, Color color)
+	{
+
+	}
+
+	CircleEnemy::~CircleEnemy()
+	{
+
+	}
+
+	void CircleEnemy::SetX(float x)
+	{
+
+	}
+
+	float CircleEnemy::GetX()
+	{
+
+	}
+
+	void CircleEnemy::SetY(float y)
+	{
+
+	}
+
+	float CircleEnemy::GetY()
+	{
+
+	}
+
+	void CircleEnemy::SetRadius(float radius)
+	{
+
+	}
+
+	float CircleEnemy::GetRadius()
+	{
+
+	}
+
+	void CircleEnemy::SetColor(Color color)
+	{
+
+	}
+
+	Color CircleEnemy::GetColor()
+	{
+
+	}
+
+	void CircleEnemy::MoveCircleEnemy()
+	{
+
+	}
+
+	void CircleEnemy::InitCircleEnemy()
+	{
+
+	}
+
+	void CircleEnemy::CircleEnemyOutOfScreen()
+	{
+
+	}
+}
--- src/gameobjects/circle_enemy.h
+++ src/gameobjects/circle_enemy.h
@@ -1,4 +1,34 @@
 #ifndef CIRCLE_ENEMY_H
 #define CIRCLE_ENEMY_H
 
+#include "raylib.h"
+
+namespace Colortrack 
+{
+	class CircleEnemy
+	{
+	private:
+		float _x;
+		float _y;
+		float _radius;
+		Vector2 _center;
+		Color _color;
+	public:
+		CircleEnemy();
+		CircleEnemy(float x, float y, float radius, Vector2 center, Color color);
+		~CircleEnemy();
+		void SetX(float x);
+		float GetX();
+		void SetY(float y);
+		float GetY();
+		void SetRadius(float radius);
+		float GetRadius();
+		void SetColor(Color color);
+		Color GetColor();
+		void MoveCircleEnemy();
+		void InitCircleEnemy();
+		void CircleEnemyOutOfScreen();
+	};
+}
+
 #endif
