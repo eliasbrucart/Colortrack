914b1e08ae3ab6fba489415b0f386e818588d860
Elias <eliasbrucart@hardgames.com.ar>
2020-11-26 20:56:34 -0300

    Added tutorial screen

--- src/screens/tutorial.cpp
+++ src/screens/tutorial.cpp
@@ -1,10 +1,17 @@
 #include "tutorial.h"
 
+#include"raylib.h"
+
+#include"game_loop.h"
+
 namespace Colortrack
 {
 	Tutorial::Tutorial()
 	{
-
+		_backToMenu.x = 300.0f;
+		_backToMenu.y = 400.0f;
+		_backToMenu.width = 100.0f;
+		_backToMenu.height = 50.0f;
 	}
 
 	Tutorial::~Tutorial()
@@ -14,11 +21,16 @@ namespace Colortrack
 
 	void Tutorial::Input()
 	{
-
+		_mouse = GetMousePosition();
+		if (CheckCollisionPointRec(_mouse, _backToMenu))
+		{
+			if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+				state = GameState::mainmenu;
+		}
 	}
 
 	void Tutorial::Draw()
 	{
-
+		DrawRectangleRec(_backToMenu, WHITE);
 	}
 }
--- src/screens/tutorial.h
+++ src/screens/tutorial.h
@@ -1,11 +1,15 @@
 #ifndef TUTORIAL_H
 #define TUTORIAL_H
 
+#include"raylib.h"
+
 namespace Colortrack 
 {
 	class Tutorial
 	{
 	private:
+		Rectangle _backToMenu;
+		Vector2 _mouse;
 	public:
 		Tutorial();
 		~Tutorial();
