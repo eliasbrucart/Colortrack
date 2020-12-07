9ab39d3d03452d1805da31a60ae31d02578e61ed
Elias <eliasbrucart@hardgames.com.ar>
2020-12-06 21:52:35 -0300

    Changed aspects in menu class

--- src/screens/menu.cpp
+++ src/screens/menu.cpp
@@ -4,47 +4,39 @@
 
 namespace Colortrack
 {
-	float xPlayButton = 270.0f;
-	float yPlayButton = 190.0f;
-	float widthPlayButton = 100.0f;
-	float heightPlayButton = 50.0f;
+	static const float buttonsInX = 270.0f;
+	static const float buttonsWidth = 100.0f;
+	static const float buttonsHeight = 50.0f;
 
-	float xTutorialButton = 270.0f;
-	float yTutorialButton = 270.0f;
-	float widthTutorialButton = 100.0f;
-	float heightTutorialButton = 50.0f;
+	static const float playButtonY = 190.0f;
 
-	float xCreditsButton = 270.0f;
-	float yCreditsButton = 350.0f;
-	float widthCreditsButton = 100.0f;
-	float heightCreditsButton = 50.0f;
+	static const float tutorialButtonY = 270.0f;
 
-	float xExitButton = 270.0f;
-	float yExitButton = 430.0f;
-	float widthExitButton = 100.0f;
-	float heightExitButton = 50.0f;
+	static const float creditsButtonY = 350.0f;
+
+	static const float exitButtonY = 430.0f;
 
 	Menu::Menu()
 	{
-		_playButton.x = xPlayButton;
-		_playButton.y = yPlayButton;
-		_playButton.width = widthPlayButton;
-		_playButton.height = heightPlayButton;
+		_playButton.x = buttonsInX;
+		_playButton.y = playButtonY;
+		_playButton.width = buttonsWidth;
+		_playButton.height = buttonsHeight;
 
-		_tutorialButton.x = xTutorialButton;
-		_tutorialButton.y = yTutorialButton;
-		_tutorialButton.width = widthTutorialButton;
-		_tutorialButton.height = heightTutorialButton;
+		_tutorialButton.x = buttonsInX;
+		_tutorialButton.y = tutorialButtonY;
+		_tutorialButton.width = buttonsWidth;
+		_tutorialButton.height = buttonsHeight;
 
-		_creditsButton.x = xCreditsButton;
-		_creditsButton.y = yCreditsButton;
-		_creditsButton.width = widthCreditsButton;
-		_creditsButton.height = heightCreditsButton;
+		_creditsButton.x = buttonsInX;
+		_creditsButton.y = creditsButtonY;
+		_creditsButton.width = buttonsWidth;
+		_creditsButton.height = buttonsHeight;
 
-		_exitButton.x = xExitButton;
-		_exitButton.y = yExitButton;
-		_exitButton.width = widthExitButton;
-		_exitButton.height = heightExitButton;
+		_exitButton.x = buttonsInX;
+		_exitButton.y = exitButtonY;
+		_exitButton.width = buttonsWidth;
+		_exitButton.height = buttonsHeight;
 	}
 
 	Menu::~Menu()
